657ba80a5fedba6c67c413971c616229a9fdb876
Karris  Black <karris.b@gmail.com>
2021-01-03 22:05:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
+  array.each do |character|
+    puts "Hello " + character
+end
+  
+  # Use `each` to end
   #
   # Print a custom greeting for each element
 end
